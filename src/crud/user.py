763e1598ecdd7b89abcf91ecdfbe2d846d155107
tommy <tommasominuto@gmail.com>
2025-02-22 16:55:45 +0100

    fix on delete user

--- src/crud/user.py
+++ src/crud/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from src.models.user import User
+from src.models.loan import Loan
 from src.schemas.user import UserCreate, UserUpdate
 from fastapi import HTTPException, status
 
@@ -27,6 +28,10 @@ def delete_user(db: Session, user_id: int):
     db_user = db.query(User).filter(User.id == user_id).first()
     if not db_user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    # Delete all loans associated with the user
+    db.query(Loan).filter(Loan.user_id == user_id).delete()
+    
     db.delete(db_user)
     db.commit()
     return db_user
